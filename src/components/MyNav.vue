5a446f0bcabd936b816804587e7802be701c9b98
岡田玄哉 <g.okada@trifort.jp>
2016-09-13 00:18:40 +0900

    ナビざっくり

---
+++ src/components/MyNav.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>トップ</h1>
+  <ul>
+    <li>ツイート</li>
+    <li>除外ワード</li>
+    <li>除外ユーザ</li>
+    <li>ピックアップユーザー</li>
+    <li>収集ワード</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Twitter管理画面',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-size: 1rem;
+}
+</style>
