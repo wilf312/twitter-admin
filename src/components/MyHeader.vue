15a32ebfa49bd62f921566d8debfd0493350c7c9
岡田玄哉 <g.okada@trifort.jp>
2016-09-13 00:18:56 +0900

    header部分

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Twitter管理画面',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  text-align: left;
+}
+</style>
