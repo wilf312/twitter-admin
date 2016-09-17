1d823300ce02b16a23bf804d7771a6ae769eccf0
岡田玄哉 <g.okada@trifort.jp>
2016-09-17 18:52:42 +0900

    トップページ（仮）作成

---
+++ src/components/Top.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="top">
+    <ul>
+      <li>除外ワード</li>
+      <li>ツイート</li>
+      <li>ピックアップユーザー</li>
+      <li>収集ワード</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Twitter管理画面',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+ul {
+  list-style-type: none;
+}
+li {
+  width: 45%;
+  height: 200px;
+  border: 1px solid black;
+  float: left;
+}
+
+li:nth-child(2n) {
+  float: right;
+}
+
+
+</style>
